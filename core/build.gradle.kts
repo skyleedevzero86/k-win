6c99378f263113238b7d095949e15ae2e37e7ba9
이경용 <sleekydz86@naver.com>
2025-02-12 18:37:34 +0900

    refactor: 설정 변경

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,39 +11,4 @@ tasks.withType<Jar> {
 dependencies {
     implementation(project(":common"))
     testImplementation(testFixtures(project(":common")))
-
-    // db
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("com.mysql:mysql-connector-j")
-
-    // JPA & Querydsl
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
-    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
-    kapt("jakarta.annotation:jakarta.annotation-api")
-    kapt("jakarta.persistence:jakarta.persistence-api")
 }
-
-// Querydsl 설정 시작
-val generated = file("src/main/generated")
-
-tasks.withType<JavaCompile> {
-    options.generatedSourceOutputDirectory.set(generated)
-}
-
-sourceSets {
-    main {
-        kotlin.srcDirs += generated
-    }
-}
-
-tasks.named("clean") {
-    doLast {
-        generated.deleteRecursively()
-    }
-}
-
-kapt {
-    generateStubs = true
-}
-// Querydsl 설정 종료
