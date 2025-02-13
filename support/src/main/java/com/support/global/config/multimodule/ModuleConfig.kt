2471fc89af17118f22fe0fe7f66244e316b03e95
이경용 <sleekydz86@naver.com>
2025-02-13 20:19:59 +0900

    feat:정리

--- support/src/main/java/com/support/global/config/multimodule/ModuleConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.support.global.config.multimodule
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ComponentScan("com.common", "com.core")
-class ModuleConfig {
-}
